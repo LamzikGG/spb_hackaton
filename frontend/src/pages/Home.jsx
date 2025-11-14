2701c24cf063e5d54a1e4dbfa626e83c45fa5f9b
Nazar Stenin <stenin797@gmail.com>
2025-11-14 15:19:05 +0300

    merge 2 branches

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+// src/pages/Home.jsx
+import '../App.css';
+
+export default function Home() {
+  return (
+    <div className="container">
+      <div className="home-container">
+        <h1>Добро пожаловать!</h1>
+        <p>Вы успешно зарегистрировались и попали на главную страницу.</p>
+        {/* Здесь позже будет ваш маскот и чат */}
+      </div>
+    </div>
+  );
+}
